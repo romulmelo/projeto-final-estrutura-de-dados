67c536c513a4cdc8bb38501086f063bc96d6d45a
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-03 23:16:21 -0300

    feat: ensure queue has been created

---
+++ src/exercises/ex02/ex02.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Queue.h"
+
+int main(void) {
+  Queue *q = create(TAM);
+
+  return 0;
+}
+
+Queue *create(int max_items) {
+  Queue *q = (Queue*) malloc(sizeof(Queue));
+
+  if (q != NULL) {
+    q->head = NULL;
+    q->tail = NULL;
+    q->max_items = max_items;
+    q->qty = 0;
+  }
+
+  return q;
+}
