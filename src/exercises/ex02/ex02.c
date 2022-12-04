0b48359e59886dff1b71f091afac8a0d7e0cd6e1
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-04 01:23:18 -0300

    feat: ensure an item is added if queue is not full

--- src/exercises/ex02/ex02.c
+++ src/exercises/ex02/ex02.c
@@ -5,6 +5,13 @@
 int main(void) {
   Queue *q = create(TAM);
 
+  insertItem(q, 1);
+  insertItem(q, 2);
+  insertItem(q, 3);
+  insertItem(q, 4);
+  insertItem(q, 5);
+  insertItem(q, 6);
+
   return 0;
 }
 
@@ -26,4 +33,28 @@ int isQueueFull(Queue *q) {
 
 int isQueueEmpty(Queue *q) {
   return q->qty == 0;
+}
+
+void insertItem(Queue *q, int number) {
+  if (isQueueFull(q)) {
+    printf("Queue is full.\n");
+    return;
+  }
+
+  Node *node = (Node*) malloc(sizeof(Node));
+
+  if (node != NULL) {
+    node->number = number;
+    node->next = NULL;
+
+    if (isQueueEmpty(q)) {
+      q->head = node;
+      q->tail = node;
+    } else {
+      q->tail->next = node;
+      q->tail = node;
+    }
+
+    q->qty++;
+  }
 }
