4261169e6dd0bf3744c592cd1e6da1588c34530e
Romulo <dev.eu.romulo.melo@gmail.com>
2022-12-05 18:40:58 -0300

    feat: implements create method interface

--- src/exercises/ex01/ex01.c
+++ src/exercises/ex01/ex01.c
@@ -3,5 +3,19 @@
 #include "Stack.h"
 
 int main(void) {
+  Stack *s = create(TAM);
+
   return 0;
+}
+
+Stack *create(int max_items) {
+  Stack *s = (Stack *) malloc(sizeof(Stack));
+
+  if (s != NULL) {
+    s->top = NULL;
+    s->max_items = max_items;
+    s->qty = 0;
+  }
+
+  return s;
 }
