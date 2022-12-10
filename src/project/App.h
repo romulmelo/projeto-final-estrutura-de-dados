22f12f420125bb9e3629e681d077bd7081062603
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-09 23:07:21 -0300

    feat: add Wine and Queue type definition

---
+++ src/project/App.h
@@ -0,0 +1,20 @@
+#define MAX 50
+
+typedef struct wine {
+  char name[MAX];
+  char birthplace[MAX];
+  char grapeType[MAX];
+  int harvest;
+} Wine;
+
+typedef struct node {
+  Wine *wine;
+  struct node *next;
+  struct node *prev;
+} Node;
+
+typedef struct queue {
+  Node *head;
+  Node *tail;
+  int qty;
+} Queue;
