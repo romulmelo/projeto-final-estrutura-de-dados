a96fc492cab4c6a8b72b2bc9724351be26337cb0
Romulo Melo <dev.eu.romulo.melo@gmail.com>
2022-12-09 23:11:39 -0300

    feat: implements createQueue method

--- src/project/app.c
+++ src/project/app.c
@@ -5,3 +5,15 @@
 int main(int argc, char const *argv[]) {
   return 0;
 }
+
+Queue *createQueue() {
+  Queue *q = (Queue*) malloc(sizeof(Queue));
+
+  if (q != NULL) {
+    q->head = NULL;
+    q->tail = NULL;
+    q->qty = 0;
+  }
+
+  return q;
+}
